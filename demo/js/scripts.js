1161b685518ac6f7878c59f3f4f6f41bc0051997
MarceloXP <marceloxp@gmail.com>
2018-07-13 16:18:52 -0300

    - update demo;

--- demo/js/scripts.js
+++ demo/js/scripts.js
@@ -1,24 +1,30 @@
-$(document).ready(function() {
-	
-	$('#frCep' ).simpleMask( { 'mask': 'cep'     , 'nextInput': true } );
-	$('#frDtel').simpleMask( { 'mask': 'ddd-tel9', 'nextInput': true } );
-	$('#frTel' ).simpleMask( { 'mask': 'tel9'    , 'nextInput': true } );
-	$('#frData').simpleMask( { 'mask': 'data'    , 'nextInput': true } );
-	$('#frCpf' ).simpleMask( { 'mask': 'cpf'     , 'nextInput': true } );
-	$('#frCnpj').simpleMask( { 'mask': 'cnpj'    , 'nextInput': true } );
+$(document).ready
+(
+	function()
+	{
+		$('#frCep' ).simpleMask( { 'mask': 'cep'     , 'nextInput': true } );
+		$('#frDtel').simpleMask( { 'mask': 'ddd-tel9', 'nextInput': true } );
+		$('#frTel' ).simpleMask( { 'mask': 'tel9'    , 'nextInput': true } );
+		$('#frData').simpleMask( { 'mask': 'data'    , 'nextInput': true } );
+		$('#frCpf' ).simpleMask( { 'mask': 'cpf'     , 'nextInput': true } );
+		$('#frCnpj').simpleMask( { 'mask': 'cnpj'    , 'nextInput': true } );
 
-	$('#frCallback').simpleMask
-	(
-		{
-			'mask'       : '#####',
-			'nextInput'  : true,
-			'onComplete' : function(element)
+		$('#frCallback').simpleMask
+		(
 			{
-				console.log('onComplete', element);
+				'mask'       : '#####',
+				'nextInput'  : true,
+				'onComplete' : function(element)
+				{
+					console.log('onComplete', element);
+				}
 			}
-		}
-	);
+		);
 
-	$('#frCep').focus();
-
-});
+		$('#frCep').focus();
+	
+		var md = $('#readme').text();
+		var html = parse(md);
+		$('#readme-html').html(html);
+	}
+);
